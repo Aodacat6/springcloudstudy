7cac335fea6516c05ad54efc1e75cf67874eeaa9
songdalin <songdalin@qdcares.cn>
2021-03-15 19:04:13 +0800

    rabbitmq

--- rabbitmq/src/main/java/com/mystudy/rabbitmq/config/DirectMQConfig.java
+++ rabbitmq/src/main/java/com/mystudy/rabbitmq/config/DirectMQConfig.java
@@ -35,6 +35,9 @@ public class DirectMQConfig {
         Map<String, Object> map = new HashMap<>();
         // 队列中的消息未被消费则30秒后过期
         map.put("x-message-ttl", 30000);
+        // 绑定该队列到私信交换机
+        map.put("x-dead-letter-exchange", "deadExchange");
+        map.put("x-dead-letter-routing-key","");
         return new Queue("Direct-Queue",true,false,false, map);
     }
 
@@ -42,5 +45,39 @@ public class DirectMQConfig {
     public Binding bindDirect(@Qualifier("directExchange") DirectExchange exchange,
                               @Qualifier("testQueue") Queue queue) {
         return BindingBuilder.bind(queue).to(exchange).with("mq.direct");
+    }
+
+    /**
+     * 死信交换机
+     * @return
+     */
+    @Bean("deadExchange")
+    public DirectExchange deaExchange() {
+        //DirectExchange(String name, boolean durable, boolean autoDelete, Map<String, Object> arguments)
+        return new DirectExchange("Dead-Exchange",true,false,null);
+    }
+
+    /**
+     * 死信队列
+     * @return
+     */
+    @Bean("deadQueue")
+    public Queue deadQueue() {
+        //String name, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
+
+        // durable:是否持久化,默认是false,持久化队列：会被存储在磁盘上，当消息代理重启时仍然存在，暂存队列：当前连接有效
+        // exclusive:默认也是false，只能被当前创建的连接使用，而且当连接关闭后队列即被删除。此参考优先级高于durable
+        // autoDelete:是否自动删除，当没有生产者或者消费者使用此队列，该队列会自动删除。
+        // arguments：设置可用配置参数
+        Map<String, Object> map = new HashMap<>();
+        // 队列中的消息未被消费则30秒后过期
+        map.put("x-message-ttl", 30000);
+        return new Queue("Dead-Queue",true,false,false, map);
+    }
+
+    @Bean
+    public Binding bindDead(@Qualifier("deadExchange") DirectExchange exchange,
+                              @Qualifier("deadQueue") Queue queue) {
+        return BindingBuilder.bind(queue).to(exchange).with("");
     }
 }
