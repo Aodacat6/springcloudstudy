ac8296f5c1771243be59cf773b8435ac800ce92f
songdalin <songdalin@qdcares.cn>
2021-03-15 11:05:16 +0800

    rabbitmq

--- rabbitmq/src/main/java/com/mystudy/rabbitmq/config/RabbitMQConfig.java
+++ rabbitmq/src/main/java/com/mystudy/rabbitmq/config/RabbitMQConfig.java
@@ -1,11 +1,12 @@
-package com.mystudy.config;
+package com.mystudy.rabbitmq.config;
 
 import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.xml.bind.Binder;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ：songdalin
@@ -30,12 +31,16 @@ public class RabbitMQConfig {
         // durable:是否持久化,默认是false,持久化队列：会被存储在磁盘上，当消息代理重启时仍然存在，暂存队列：当前连接有效
         // exclusive:默认也是false，只能被当前创建的连接使用，而且当连接关闭后队列即被删除。此参考优先级高于durable
         // autoDelete:是否自动删除，当没有生产者或者消费者使用此队列，该队列会自动删除。
-        return new Queue("Direct-Exchange",true,false,false,null);
+        // arguments：设置可用配置参数
+        Map<String, Object> map = new HashMap<>();
+        // 队列中的消息未被消费则30秒后过期
+        map.put("x-message-ttl", 30000);
+        return new Queue("Direct-Queue",true,false,false, map);
     }
 
     @Bean
     public Binding bindDirect(@Qualifier("directExchange") DirectExchange exchange,
                               @Qualifier("testQueue") Queue queue) {
-        return BindingBuilder.bind(queue).to(exchange).with("");
+        return BindingBuilder.bind(queue).to(exchange).with("mq.direct");
     }
 }
---
+++ rabbitmq/src/main/java/com/mystudy/rabbitmq/controller/MqController.java
@@ -0,0 +1,43 @@
+package com.mystudy.rabbitmq.controller;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageDeliveryMode;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：songdalin
+ * @date ：2021-03-12 下午 05:48
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+@RestController
+@RequestMapping("")
+public class MqController {
+
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/test")
+    public void test() {
+
+        //消息过期方式二：给每一条消息设置过期时间
+        MessageProperties properties = new MessageProperties();
+        //延迟发送
+        //properties.setDelay(10000);
+        //每条消息过期时间
+        properties.setExpiration("10000");
+        byte[] bytes = "这是一条测试消息".getBytes();
+        Message message = new Message(bytes, properties);
+        //  路由key，如果不对，不能发布到队列上
+        //rabbitTemplate.convertAndSend("Direct-Exchange", "mq.direct", "这是一条测试消息!!!");
+        rabbitTemplate.convertAndSend("Direct-Exchange", "mq.direct", message);
+        System.out.println("发送完毕！！！");
+    }
+}
