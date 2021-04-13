1b565ee4ec318a0c07acdc4a07fcd03aef64c824
songdalin <songdalin@qdcares.cn>
2021-04-13 18:19:46 +0800

    feign修改

--- producer-service/src/main/java/com/mystudy/producer/configuration/RibbonConfig.java
+++ producer-service/src/main/java/com/mystudy/producer/configuration/RibbonConfig.java
@@ -1,7 +1,7 @@
 package com.mystudy.producer.configuration;
 
 import com.mystudy.producer.MyRibbonRule;
-import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,7 +18,19 @@ public class RibbonConfig {
     @Bean
     public IRule iRule () {
         //返回负载策略
+        //一、随机
         //return new RandomRule();
+        //二、轮询
+        //return new RoundRobinRule();
+        //三、可用过滤策略
+        //return new AvailabilityFilteringRule();
+        //四、响应时间权重策略
+        //return new WeightedResponseTimeRule();
+        //五、轮询失败重试策略
+        //return new RetryRule();
+        //六、并发量最小可用策略
+        //return new BestAvailableRule();
+
         return new MyRibbonRule();
     }
 }
